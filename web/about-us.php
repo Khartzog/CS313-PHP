30ee7fcd82b378934f8adc5979f2afa9a84efaf4
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-04-30 16:46:02 -0600

    changed my about-us

---
+++ web/about-us.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div> 
+    <h1>Welcome to the Viztolio About Us Page</h1>
+
+    <p>We are a small start up company coming straight out of College,
+    and we aspire to change the world in many ways. Our aim is to make 
+    a better programming langue that will upsurp Python any old day.
+    Enjoy! And Support if you wanna. <p>
+    </div>
+
+   
+</body>
+</html>
