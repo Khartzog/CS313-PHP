13a43ff0eafa6cae57c75131c0a6c43bb7c7a6c0
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-04-30 16:40:57 -0600

    Added files

---
+++ web/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    <input type="button" id="Administrator" name="admin" value="Login "/>
+    <br>
+    <button type="button" id="Tester" name="tester">Log in as Tester</button>
+    
+</body>
+</html> 
