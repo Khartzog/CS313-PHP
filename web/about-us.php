b203f16ec2a92a2898d2ceded55d328033c04ca4
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-01 19:51:34 -0600

    added nav bar

--- web/about-us.php
+++ web/about-us.php
@@ -5,17 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
 </head>
 <body>
-    
-    <div> 
+<ul>
+  <li><a class="active" href="home.php">Home</a></li>
+  <li><a href="login.php">Login</a></li>
+  <li><a href="about-us.php">About Us</a></li>
+</ul>
     <h1>Welcome to the Viztolio About Us Page</h1>
+    
 
     <p>We are a small start up company coming straight out of College,
     and we aspire to change the world in many ways. Our aim is to make 
     a better programming langue that will upsurp Python any old day.
     Enjoy! And Support if you wanna. <p>
-    </div>
+ 
 
    
 </body>
