bde1106ca52dad3b6882cb51cc588a2672aac82c
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-04 23:18:37 -0600

    starting assignment 2

--- web/Assignment-02/homePage.php
+++ web/Assignment-02/homePage.php
@@ -20,7 +20,7 @@
   </div>
   <h3>Hi! I'm Kaylee</h3>
   <p>I am a Junior in College, I am 21 years old, and I served a mission in Ecuador.
-  <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fecuador.travel%2Fwp-content%2Fuploads%2F2018%2F10%2Fportada-moviles-768x384.jpg&imgrefurl=https%3A%2F%2Fecuador.travel%2Fen%2F&docid=sijoL5fBdWi5rM&tbnid=GIUB-PlUqJRJvM%3A&vet=10ahUKEwiHi_jby4PiAhWCqp4KHeRYBSgQMwh0KAowCg..i&w=768&h=384&bih=568&biw=1226&q=ecuador&ved=0ahUKEwiHi_jby4PiAhWCqp4KHeRYBSgQMwh0KAowCg&iact=mrc&uact=8" 
+  <img src="cinqueterre.jpg" 
   class="img-rounded" alt="Cinque Terre" width="304" height="236">
 </div>
 </body>
