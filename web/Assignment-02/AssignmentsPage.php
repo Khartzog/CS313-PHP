666ad552a29fde400747c24fc8aa281d4a05fe39
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-04 23:10:55 -0600

    starting assignment 2

--- web/Assignment-02/AssignmentsPage.php
+++ web/Assignment-02/AssignmentsPage.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php 
+    include 'navBar.php';
+    ?>
 </head>
 <body>
 <div class="container">
     <h1>Assignments Page</h1>
     <div class="list-group">
-    <a href="#" class="list-group-item" data-toggle="tooltip" data-placement="top" title="Coming Soon!">Coming Soon</a>
-    <a href="#" class="list-group-item" data-toggle="tooltip" data-placement="top" title="Coming Soon!">Coming Soon</a>
     <a href="#" class="list-group-item" data-toggle="tooltip" data-placement="top" title="Coming Soon!">Coming Soon</a>
   </div>
 </div>
