e552a5b22d4fd4f571b0dbe42e60a3c6c55e2c3e
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-11 19:25:22 -0600

    nA

--- web/Assignment-03/browseItems.php
+++ web/Assignment-03/browseItems.php
@@ -15,10 +15,10 @@
     <p>Ready to do something fun this Summer? Its time to get a book</p>
     <a href="" class="btn btn-info" role="button">Cart</a>
   </div>
-  <div class="d-flex align-items-stretch bg-light" style="height:150px">
-    <div class="p-2 border"><img src="harry-potter.jpg" class="img-rounded" alt="Harry Potter" width="150" height="240"></div>
-    <div class="p-2 border">Flex item 2</div>
-    <div class="p-2 border">Flex item 3</div>
+  <div class="d-flex align-items-stretch bg-light" style="height:450px">
+    <div><img src="harry-potter.jpg" class="img-rounded" alt="Harry Potter" width="150" height="240"></div>
+    <div class="p-2 border"><b>Price: $20.99</b></div>
+    <div class="p-2 border"><button type="button" class="btn btn-success">Add To Cart</button></div>
   </div>
   <img src="Hunger-Games.jpg"
   class="img-rounded" alt="Hunger Games" width="150" height="240"><br>
