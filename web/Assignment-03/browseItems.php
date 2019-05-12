178315b1f5ecaa7d3acf5ce4e311efe3487ab863
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-11 19:48:49 -0600

    nA

--- web/Assignment-03/browseItems.php
+++ web/Assignment-03/browseItems.php
@@ -17,19 +17,34 @@
   </div>
   <div class="d-flex align-items-stretch bg-light" style="height:250px">
     <div><img src="harry-potter.jpg" class="img-rounded" alt="Harry Potter" width="150" height="240"></div>
-    <div class="align-self-center"><b><pre>  Price: $20.99</pre></b></div>
+    <div class="align-self-center"><b><pre>  Price: $20.99   </pre></b></div>
     <div class="align-self-center"><button type="button" class="btn btn-success">Add To Cart</button></div>
   </div>
-  <img src="Hunger-Games.jpg"
-  class="img-rounded" alt="Hunger Games" width="150" height="240"><br>
-  <img src="Fault-in-our-Stars.jpg"
-  class="img-rounded" alt="The Fault in our Stars" width="150" height="240"><br>
-  <img src="Lord-of-the-Rings.jpg"
-  class="img-rounded" alt="Lord of the Rings" width="150" height="240"><br>
-  <img src="City-Of-Bones.jpg"
-  class="img-rounded" alt="City of Bones" width="150" height="240"><br>
-  <img src="Percy-Jackson.jpg"
-  class="img-rounded" alt="Percy Jackson" width="150" height="240"><br>
+  <div class="d-flex align-items-stretch bg-light" style="height:250px">
+    <div><img src="Hunger-Games.jpg" class="img-rounded" alt="Hunger Games" width="150" height="240"></div>
+    <div class="align-self-center"><b><pre>  Price: $21.99   </pre></b></div>
+    <div class="align-self-center"><button type="button" class="btn btn-success">Add To Cart</button></div>
+  </div>
+  <div class="d-flex align-items-stretch bg-light" style="height:250px">
+    <div><img src="Fault-in-our-Stars.jpg" class="img-rounded" alt="The Fault in our Stars" width="150" height="240"></div>
+    <div class="align-self-center"><b><pre>  Price: $22.99   </pre></b></div>
+    <div class="align-self-center"><button type="button" class="btn btn-success">Add To Cart</button></div>
+  </div>
+  <div class="d-flex align-items-stretch bg-light" style="height:250px">
+    <div><img src="Lord-of-the-Rings.jpg" class="img-rounded" alt="Lord of the Rings" width="150" height="240"></div>
+    <div class="align-self-center"><b><pre>  Price: $19.99   </pre></b></div>
+    <div class="align-self-center"><button type="button" class="btn btn-success">Add To Cart</button></div>
+  </div>
+  <div class="d-flex align-items-stretch bg-light" style="height:250px">
+    <div><img src="City-Of-Bones.jpg" class="img-rounded" alt="City of Bones" width="150" height="240"></div>
+    <div class="align-self-center"><b><pre>  Price: $20.99   </pre></b></div>
+    <div class="align-self-center"><button type="button" class="btn btn-success">Add To Cart</button></div>
+  </div>
+  <div class="d-flex align-items-stretch bg-light" style="height:250px">
+    <div><img src="Percy-Jackson.jpg" class="img-rounded" alt="Percy Jackson" width="150" height="240"></div>
+    <div class="align-self-center"><b><pre>  Price: $18.99   </pre></b></div>
+    <div class="align-self-center"><button type="button" class="btn btn-success">Add To Cart</button></div>
+  </div>
 </div>    
 </body>
 </html>
