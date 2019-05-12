f8de142ffed7d063a189282e43279bed68eeeb75
Kaylee Hartzog <kayleehartzog@205-185-99-96.directcom.com>
2019-05-11 23:10:40 -0600

    nA

--- web/Assignment-03/checkOut.php
+++ web/Assignment-03/checkOut.php
@@ -20,12 +20,15 @@
   <form action="confirmationPage.php">
     <label for="fname">First Name</label>
     <input type="text" id="fname" name="firstname" placeholder="Your name..">
-
+<br>
     <label for="lname">Last Name</label>
     <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
+<br>
     <label for="address">Address</label>
     <input type="text" id="address" name="Address" placeholder="Your address..">
+    <br>
+    <input type="submit" value="Submit">
+  </form>
 </div>
 </body>
 </html>
