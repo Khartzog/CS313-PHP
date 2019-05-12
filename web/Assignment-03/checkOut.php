95ff79a46be5aef18a8528b6d72487d1b9e3b498
Kaylee Hartzog <kayleehartzog@205-185-99-96.directcom.com>
2019-05-11 23:05:44 -0600

    nA

--- web/Assignment-03/checkOut.php
+++ web/Assignment-03/checkOut.php
@@ -5,8 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+<div class="container">
+  <div class="jumbotron">
+    <h1>Check Out</h1>      
+    <a href="viewCart.php" class="btn btn-info" role="button">Back to Cart</a>
+    <br>
+    <br>
+    <a href="confirmationPage.php" class="btn btn-info" role="button">Purchase</a>
+  </div>
 </body>
 </html>
