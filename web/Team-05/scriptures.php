fecc792cd543cc62dbf27238c94eeac5683578aa
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-21 16:24:16 -0600

    did thing

---
+++ web/Team-05/scriptures.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+  <div class="jumbotron">
+    <h1>Scripture Resources</h1> 
+  </div>
+
+</div>
+
+<?php
+  try{
+    $dbUrl = getenv('postgres://hasznbfctoolrm:615ad5e3cd7d104d0a5037b931a568f6aa9d27ee36cd93e635a8582f0f9db109@ec2-184-72-237-95.compute-1.amazonaws.com:5432/dd7iid19njq5jo
+    ');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+{
+  echo 'Error Connecting!';
+  die();
+}
+
+foreach ($db->query('SELECT book, chapter, verse, scripture_content FROM scriptures') as $row)
+{
+  echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - '" . $row['scripture_content'] . "'";
+  echo '<br/>';
+}
+?>
+</body>
+</html>
