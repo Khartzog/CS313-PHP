37c9cc6d74990dc79d273dbf646147f938f4a60d
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-06-02 17:43:54 -0600

    g

--- web/Assignment-05/journalHome.php
+++ web/Assignment-05/journalHome.php
@@ -1,7 +1,4 @@
-<!-- <?php
-require "connect.php";
-$db = get_db();
-?>  -->
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,8 +7,7 @@ $db = get_db();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   
 </head>
 <body>
 <h1>Please Enter User Name</h1><br>
