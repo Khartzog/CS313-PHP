d5f7f82ba55cd41b1a84a7c190c9e91d8e504716
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-06-04 14:44:35 -0600

    a

--- web/Assignment-05/showEntries.php
+++ web/Assignment-05/showEntries.php
@@ -20,8 +20,8 @@ try{
   die();
 }
 $username = $_GET['username'];
-
-echo '<br/>';
+echo '<h1>Welcome ' . $username . '</h1><br><h3>Here are your entries:</h3>';
+echo '<br><br>';
 $userid = NULL;
 //$userid = $db->query('SELECT userid FROM userinfo WHERE username == $username');
 //echo $userid;
@@ -35,6 +35,15 @@ foreach ($db->query('SELECT userid, username FROM userinfo') as $row)
 foreach ($db->query('SELECT entry_id, entry_content, entry_date, userid, journal_id, media_id FROM user_entry') as $row)
 {
   if ($row['userid'] == $userid) {
+    $journalName = NULL;
+    $journalId = $row['journal_id'];
+    foreach ($db->query('SELECT journal_id, journal_name FROM journal') as $row)
+    {
+      if ($row['journal_id'] == $journalID) {
+        $journalName = $row['journal_name'];
+      }
+    }   
+    echo $journalName . '<br>' . $row['entry_date'] . '<br><br>';
     echo $row['entry_content'];
   }
 }
