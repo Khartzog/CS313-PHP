bd6109d6b15105a5e4e30dbc984c3cff46161ec6
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-06-08 16:04:20 -0600

    a

--- web/Assignment-06/newEntry.php
+++ web/Assignment-06/newEntry.php
@@ -1,4 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+   
+</head>
+<body>
 <?php
-    echo '<center>' . date("Y-m-d") . "</center><br>";
-    
+    echo date("Y-m-d") . "<br>";
+
 ?>
+
+<form name="form" action="confirmEntry.php" method="get">
+<input type='textarea' name='content' id='content'><br>
+<h4>Which journal will you add this entry to?</h4><br>
+<input type='text' name='journal' id='journal'><br>
+<input type="submit">
+</form>
+    
+</body>
+</html>
