fdd8a4b54935e4211952097751b01bd4d815e58b
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-06-08 21:03:55 -0600

    a

---
+++ web/Assignment-06/confirmEntry.php
@@ -0,0 +1,39 @@
+<?php
+try{
+        $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $ex)
+    {
+      echo 'Error Connecting!';
+      die();
+    }
+    $journalName = $_GET['journal'];
+    $isJournal = 'False';
+
+    foreach ($db->query('SELECT journal_name FROM journal') as $row)
+{
+  if ($row['journal_name'] == $journalName) {
+    $isJournal = 'True';
+  }
+}
+
+    if ($isJournal == 'False') {
+        echo '<center><h4>Invalid Journal Name</h4></center><br>';
+        echo '<p>Please enter a journal name.</p>';
+        echo '<form action="confirmEntry.php">
+        <input type="text" name="journal" id="journal"><br>
+        </form>';
+    }
+    ?>
