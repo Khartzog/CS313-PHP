1f2026ff5ff4f87ce4d962aa3bc04e2a90b6baab
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-06-09 11:44:43 -0600

    a

--- web/Assignment-06/confirmEntry.php
+++ web/Assignment-06/confirmEntry.php
@@ -22,25 +22,40 @@ try{
     }
     $journalName = $_GET['journal'];
     $isJournal = 'False';
-
+    
+    
     foreach ($db->query('SELECT journal_id, journal_name FROM journal') as $row)
-{
-  if ($row['journal_name'] == $journalName) {
-    $isJournal = 'True';
-    $journalId = $row['journal_id'];
-    $_SESSION['journal_Id'] = $journalId;
-  }
-}
-
+    {
+      if ($row['journal_name'] == $journalName) {
+        $isJournal = 'True';
+        $journalId = $row['journal_id'];
+        $_SESSION['journal_Id'] = $journalId;
+      }
+    }
+    
     if ($isJournal == 'False') {
+      $content = $_GET['content'];
+      echo $content;
         echo '<center><h4>Invalid Journal Name</h4></center><br>';
         echo '<p>Please enter a journal name.</p>';
         echo '<form action="confirmEntry.php">
         <input type="text" name="journal" id="journal"><br>
+        <input type="hidden" id="content" name="content" value=$_GET["content"]>
         <button type="submit">Make Entry</button><br>
         </form>';
     }
     else if($isJournal == 'True') {
-      
-    }
+      echo '<center><h1>Entry Added</h1></center>';
+
+      $userID = $_SESSION['user_id'];
+      echo $content;
+
+    //   $query = 'INSERT INTO user_entry(entry_content, entry_date, userid, journal_id) VALUES(:entry_content, now(), :userid, :journal_id)';
+	  //   $statement = $db->prepare($query);
+
+  	//   $statement->bindValue(':entry_content', $content);
+	  //   $statement->bindValue(':userid', $userID);
+	  //   $statement->bindValue(':journal_id', $journalId);
+	  //   $statement->execute();
+    // }
     ?>
