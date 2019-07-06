ccd5a4eeb941b98a5a68b383a83a70a68f45df73
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-06 08:55:01 -0600

    a

--- web/Project-01/firstPage.php
+++ web/Project-01/firstPage.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-   
+   <style>
+       body {
+           background-image: url("journal.jpg");
+       }
+   </style>
 </head>
 <body>
 <div>
+    <br><br><br><br><br>
     <center><form action="signIn.php">
     <button type="submit">Sign In</button>
     <button type="submit" formaction="signUp.php">Sign Up</button>
