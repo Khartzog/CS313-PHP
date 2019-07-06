1e63900d3603fcbb70893beafba28a6a7c4b7f5f
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-06 09:05:48 -0600

    a

--- web/Project-01/firstPage.php
+++ web/Project-01/firstPage.php
@@ -10,14 +10,25 @@
            background-image: url("journal.jpg");
            background-blend-mode: lighten;
        }
+
+       .block {
+        display: block;
+        width: 100%;
+        border: none;
+        background-color: #4CAF50;
+        padding: 14px 28px;
+        font-size: 16px;
+        cursor: pointer;
+        text-align: center;
+        }
    </style>
 </head>
 <body>
 <div>
     <br><br><br><br><br>
     <center><form action="signIn.php">
-    <button type="submit">Sign In</button>
-    <button type="submit" formaction="signUp.php">Sign Up</button>
+    <button type="submit" class="block">Sign In</button>
+    <button type="submit" formaction="signUp.php" class="block">Sign Up</button>
     </form></center>
     </div>
 </body>
