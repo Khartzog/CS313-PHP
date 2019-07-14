a1b7b43aa0bbc17abc56f7032596bf13a6a534c2
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-13 20:22:39 -0600

    a

--- web/Project-01/viewEntries.php
+++ web/Project-01/viewEntries.php
@@ -29,10 +29,10 @@ echo '<center><h1>Here are your entries</h1></center><br>';
 $joinedResults = $db->prepare("SELECT eu.*, j.journal_name FROM user_entry eu LEFT JOIN journal j ON eu.journal_id = j.journal_id WHERE eu.userid = $userID");
 $joinedResults->execute();
 while ($row = $joinedResults->fetch(PDO::FETCH_ASSOC)){
-$journalName = $row[' . journal_name . '];
-$date = $row[' . entry_date . '];
-$content = $row[' . entry_content . '];
-$entry_id = $row[' . entry_id . '];
+$journalName = $row['journal_name'];
+$date = $row['entry_date'];
+$content = $row['entry_content'];
+$entry_id = $row['entry_id'];
 echo '<input type="radio" name="entry" value="' . $entry_id . '">' . $date . '<br>' . $content . '<br><br>';
 };      
 // echo '<button type="submit">Delete Entry</button><br>';
