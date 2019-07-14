e06fae762ec4ccc669f0a3bc51118cd72e0f1217
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-14 10:19:02 -0600

    a

---
+++ web/Project-01/deleteEntry.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+try{
+    $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+{
+  echo 'Error Connecting!';
+  die();
+}
+echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
+echo '<div class="textArea">';
+echo '<center><h1>Entry Deleted</h1></center>';
+
+echo '</div>';
+$userid = $_GET['entry'];
+
+$query = 'DELETE FROM user_entry WHERE userid = $userid';
+$statement = $db->prepare($query);
+
+// $statement->bindValue(':entry_content', $content);
+// $statement->bindValue(':userid', $userID);
+// $statement->bindValue(':journal_id', $journalId);
+$statement->execute();
+
+echo '<center><form action="viewEntries.php">
+        <button type="submit">View Entries</button><br>
+        </form></center>';
+
+?>
