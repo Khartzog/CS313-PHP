eaa4ed1545f558fa6b29ab4427f6a9c6e1755399
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-13 20:16:15 -0600

    a

--- web/Project-01/viewEntries.php
+++ web/Project-01/viewEntries.php
@@ -25,16 +25,23 @@ echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
 echo '<div class="textArea">';
 echo '<center><h1>Here are your entries</h1></center><br>';
 
-$joinedResults = $db->prepare("SELECT eu.*, j.journal_name FROM user_entry eu LEFT JOIN journal j ON eu.journal_id = j.journal_id WHERE eu.userid = $userID");
-$joinedResults->execute();
+echo '<form action="deleteEntry.php">';
+echo '<?php';
+echo '$joinedResults = $db->prepare("SELECT eu.*, j.journal_name FROM user_entry eu LEFT JOIN journal j ON eu.journal_id = j.journal_id WHERE eu.userid = $userID");';
+echo '$joinedResults->execute();';
+echo 'while ($row = $joinedResults->fetch(PDO::FETCH_ASSOC)){';
+echo '$journalName = $row[' . journal_name . '];';
+echo '$date = $row[' . entry_date . '];';
+echo '$content = $row[' . entry_content . '];';
+echo '$entry_id = $row[' . entry_id . '];';
+echo '<input type="radio" name="entry" value="' . $entry_id . '">' . $date . '<br>' . $content . '<br><br>';
+echo '}';
+echo '?>';
+        
+echo '<button type="submit">Delete Entry</button><br>';
+echo '</form>';
+echo '</div>';
 
-while ($row = $joinedResults->fetch(PDO::FETCH_ASSOC)){
-  $journalName = $row['journal_name'];
-  $date = $row['entry_date'];
-  $content = $row['entry_content'];
-  $entry_id = $row['entry_id'];
-  echo '<input type="radio" name="entry" value="' . $entry_id . '">' . $date . '<br>' . $content . '<br><br>';
-}
 $username = $_SESSION['user_name'];
 echo '<center><form action="mainPage.php">
         <input type="hidden" id="username" name="username" value="' . $username . '">
