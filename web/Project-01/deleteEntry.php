a0bb63f95451316b156c66b04789a5ff635a9722
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-14 10:21:30 -0600

    a

--- web/Project-01/deleteEntry.php
+++ web/Project-01/deleteEntry.php
@@ -20,23 +20,23 @@ try{
   echo 'Error Connecting!';
   die();
 }
-echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
-echo '<div class="textArea">';
-echo '<center><h1>Entry Deleted</h1></center>';
-
-echo '</div>';
 $userid = $_GET['entry'];
 
 $query = 'DELETE FROM user_entry WHERE userid = $userid';
 $statement = $db->prepare($query);
 
+$statement->execute();
 // $statement->bindValue(':entry_content', $content);
 // $statement->bindValue(':userid', $userID);
 // $statement->bindValue(':journal_id', $journalId);
-$statement->execute();
+echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
+echo '<div class="textArea">';
+echo '<center><h1>Entry Deleted</h1></center>';
+
 
 echo '<center><form action="viewEntries.php">
-        <button type="submit">View Entries</button><br>
-        </form></center>';
+<button type="submit">View Entries</button><br>
+</form></center>';
 
+echo '</div>';
 ?>
